5e282190ffcb6d4f513677f255ac5a7e7fc4b144
Adrian Czarnecki <adrian-czarnecki@live.com>
2020-02-08 19:26:03 -0500

    [DEV-31] Adds a registration view

---
+++ sprint1/androidApp/app/src/main/java/com/developersOfTheMillennium/motm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.developersOfTheMillennium.motm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page_fragment);
+    }
+}

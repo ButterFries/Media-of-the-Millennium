9ea4031f27391d49c4f64e0c4e0018674c7badc9
gurashishArneja <59981227+gurashishArneja@users.noreply.github.com>
2020-02-13 22:34:44 -0500

    UserProfile working
    Made the User Profile page working seamless with other components of the app.

---
+++ app/app/src/main/java/com/developersOfTheMillennium/motm/UserProfilePageFragment.java
@@ -0,0 +1,18 @@
+package com.developersOfTheMillennium.motm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class UserProfilePageFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.user_profile_fragment, container, false);
+    }
+
+}

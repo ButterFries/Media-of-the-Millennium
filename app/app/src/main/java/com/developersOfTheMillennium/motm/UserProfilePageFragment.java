569263850a8f38c6079170777642e8c7c76c7d7d
gurashishArneja <59981227+gurashishArneja@users.noreply.github.com>
2020-02-13 22:47:22 -0500

    Made some changes in User Profile
    Add some things extra such as comments for future references.

--- app/app/src/main/java/com/developersOfTheMillennium/motm/UserProfilePageFragment.java
+++ app/app/src/main/java/com/developersOfTheMillennium/motm/UserProfilePageFragment.java
@@ -13,6 +13,7 @@ public class UserProfilePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.user_profile_fragment, container, false);
+        // Need to add button functioning here.
     }
 
 }

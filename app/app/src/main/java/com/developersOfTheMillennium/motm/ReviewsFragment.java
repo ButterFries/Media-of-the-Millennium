9d66b78683907d924a4c019d9ffb5815bb7d78e9
brandonmelend141 <59889949+brandonmelend141@users.noreply.github.com>
2020-02-23 21:57:22 -0500

    review_update
    added some functionality, trying to plan out the function

--- app/app/src/main/java/com/developersOfTheMillennium/motm/ReviewsFragment.java
+++ app/app/src/main/java/com/developersOfTheMillennium/motm/ReviewsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,7 +17,30 @@ public class ReviewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.reviews_page_fragment, container, false);
+
+        View v =  inflater.inflate(R.layout.reviews_page_fragment, container, false);
+        final Button submit_review = v.findViewById(R.id.button);
+
+
+
+        final EditText review = v.findViewById(R.id.review_text);
+        final String review_checker = review.getText().toString();
+        if(review_checker.isEmpty() == true){
+            review.setError("No review has been made, can not submit");
+        }else{
+            submit_review.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    //Take to Trending Review
+
+                    review.setText(review_checker);
+                    submit_review.setError("Not an error lol ");
+                }
+            });
+        }
+
+
+        return v;
     }
+
 
 }

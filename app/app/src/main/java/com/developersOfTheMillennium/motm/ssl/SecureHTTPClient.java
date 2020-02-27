fe39491d540a40c5dad91599c30787dfbea62238
Adrian Czarnecki <adrian-czarnecki@live.com>
2020-02-27 13:25:58 -0500

    DEV-32 Hotfix - updated comments

--- app/app/src/main/java/com/developersOfTheMillennium/motm/ssl/SecureHTTPClient.java
+++ app/app/src/main/java/com/developersOfTheMillennium/motm/ssl/SecureHTTPClient.java
@@ -134,8 +134,6 @@ public class SecureHTTPClient
    * file that gets bundled with the application.
    */
   private InputStream trustedCertificatesInputStream() {
-    // PEM files for root certificates of Comodo and Entrust. These two CAs are sufficient to view
-    // https://publicobject.com (Comodo) and https://squareup.com (Entrust). But they aren't
     // sufficient to connect to most HTTPS sites including https://godaddy.com and https://visa.com.
     // Typically developers will need to get a PEM file from their organization's TLS administrator.
     //SELF SIGNED CERTIFICATE

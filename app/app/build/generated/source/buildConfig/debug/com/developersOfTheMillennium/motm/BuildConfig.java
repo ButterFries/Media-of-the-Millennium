dcb80315ae345ec6fe6fd9971e6f9835229bb508
Geoffrey J P, DelaVega Ruest <geoffrey.ruest@gmail.com>
2020-04-04 10:30:25 -0400

    merge dev into master
    (not tested for issues from solving conflicts)

---
+++ app/app/build/generated/source/buildConfig/debug/com/developersOfTheMillennium/motm/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.developersOfTheMillennium.motm;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.motm";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
